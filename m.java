b4fda60bcb7ddd8369d85572bbfe41b42669cf89
Rishu Kumari <rishupandey8935@gmail.com>
2025-01-15 23:35:50 +0530

    java

---
+++ m.java
@@ -0,0 +1,6 @@
+public class m {
+
+    public static void main (String a[] ) {
+        System.out.println("hello");
+    }
+}
---
+++ ris.java
@@ -0,0 +1,5 @@
+public class ris {
+    public static void main(String[] args) {
+        System.out.println("my");
+    }
+}
